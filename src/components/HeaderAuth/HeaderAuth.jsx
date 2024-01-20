405f11b573efb62b53ea8f259ec63dc1e4543f04
Danil Baidak <lazya142425@gmail.com>
2024-01-20 20:39:26 +0300

    fix БЭМ

--- src/components/HeaderAuth/HeaderAuth.jsx
+++ src/components/HeaderAuth/HeaderAuth.jsx
@@ -19,12 +19,12 @@ export default function HeaderAuth() {
                 <Link to={'/'} className='header-auth__home'><img src={headerLogo} alt="Логотип сайта" /></Link>
                 <nav className={`header-auth__container ${isOpen ? 'header-auth__container_open' : ''}`}>
                     <div className='header-auth__container-account'>
-                        <div className='header-auth__container-account_type_films'>
+                        <div className='header-auth__container-films'>
                             <Link to={'/'} className='header-auth__link-to-home'>Главная</Link>
                             <Link to={'/movies'} className={`header-auth__link-to-films ${pathname === '/movies' ? 'header-auth__link-to-films_type_active' : ''}`}>Фильмы</Link>
                             <Link to={'/saved-movies'} className={`header-auth__link-to-films ${pathname === '/saved-movies' ? 'header-auth__link-to-films_type_active' : ''}`}>Сохранённые&nbsp;фильмы</Link>
                         </div>
-                        <div className='header-auth__container-account_type_profile'>
+                        <div className='header-auth__container-profile'>
                             <Link className='header-auth__account' to={'/profile'}>Аккаунт</Link>
                             <div className='header-auth__background'><div className='header-auth__account-icon'></div></div>
                         </div>
--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -9,7 +9,7 @@ export default function SearchForm() {
                 <form className='searchForm__form'>
                     <fieldset className='searchForm__fieldset'>
                         <img src={searchForm__icon} alt="иконка" className='searchForm__icon' />
-                        <input type="text" className='searchForm__input' placeholder='Фильм' />
+                        <input name='film' type="text" className='searchForm__input' placeholder='Фильм' />
                         <button className='searchForm__search-button'>Найти</button>
                     </fieldset>
                 </form>
