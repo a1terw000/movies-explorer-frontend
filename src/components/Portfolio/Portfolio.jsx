520906e5e6591dd1a8f044139d43fba9fe5ad3d3
Danil Baidak <lazya142425@gmail.com>
2024-03-26 20:49:22 +0300

    1

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -9,7 +9,7 @@ export default function Portfolio() {
                 <h2 className='portfolio__title'>Портфолио</h2>
                 <ul className='portfolio__list'>
                     <li className='portfolio__item'>
-                        <Link to={'#'} className='portfolio__link' target='_blank'>
+                        <Link to={'https://a1terw000.github.io/how-to-learn/'} className='portfolio__link' target='_blank'>
                             <p className='portfolio__text'>Статичный сайт</p>
                             <img alt='link-to-site' src={portfolioLink} className='portfolio__image'></img>
                         </Link>
@@ -21,7 +21,7 @@ export default function Portfolio() {
                         </Link>
                     </li>
                     <li className='portfolio__item'>
-                        <Link className='portfolio__link' to={'https://a1terw000.github.io/russian-travel/#'} target='_blank'>
+                        <Link className='portfolio__link' to={'https://lazya.nomoredomainsmonster.ru/'} target='_blank'>
                             <p className='portfolio__text'>Одностраничное приложение</p>
                             <img alt='link-to-site' src={portfolioLink} className='portfolio__image'></img>
                         </Link>
